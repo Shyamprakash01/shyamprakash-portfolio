7888ba70ebd1315ee48b31ee9d59aef2ba557cd0
Shyamprakash01 <pshyam506@gmail.com>
2023-07-12 10:53:10 +0530

    completed portfolio

---
+++ src/components/ContactUs.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return <section id="contact">
+        <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">
+              Get In Touch I’m interested in learning opportunities
+              especially ambitious or interesting open-source projects.
+              However, if you have other request or question, don’t hesitate
+              contact.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <h4>
+                Linked in :
+                https://www.linkedin.com/in/shyam-prakash-a3025b237 
+              </h4>
+              <h4>Mail Me : {resumeData.Mail}</h4>
+            </div>
+          </aside>
+        </div>
+      </section>;
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,123 @@
+let resumeData = {
+  imagebaseurl: "https://www.linkedin.com/in/shyam-prakash-a3025b237",
+  name: "Shyamprakash",
+  role: "Full Stack Developer",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and backend development and I like to learn about new technologies, write technical articles and I seek challenging opportunities where I can fully use my skills for the success of the organization",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/shyam-prakash-a3025b237",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/rbhatia46",
+      className: "fa fa-github",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/sam_snoopy_____/",
+      className: "fa fa-instagram",
+    },
+  ],
+  aboutme:
+    "I’m a aspiring software engineer specializing in building websites from scratch and implementing new features and digital experiences. Currently, I’m focused on building accessible, userfriendly, smooth websites and developing new skills around web3 technologies.",
+  address: " Big Bazaar Street Paramakudi Ramanathapuram District",
+  website: "https://rbhatia46.github.io",
+  Mail: "pshyam506@gmail.com shyamprakashtn63@gmail.com",
+  education: [
+    {
+      UniversityName: "Alagappa University",
+      specialization: "Master of Science in Computer Science",
+      MonthOfPassing: "Aug 2020",
+      YearOfPassing: "June 2022",
+      Description: "83.3%",
+    },
+    {
+      UniversityName: "Alagappa University",
+      specialization: "Bachelor of Science in Computer Science",
+      MonthOfPassing: "July 2017",
+      YearOfPassing: "May 2020",
+      Description: "78%",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "KDP Tec",
+      specialization: "Software Engineer",
+      YearOfStarting: "Sep2022",
+      YearOfLeaving: "May2023",
+      Description:
+        " Worked as Full-Stack Developer on real time Role-Based Application used to analyze and monitor the stats of an Organization with Dynamic alternative UI for the roles Developed an IOS/Android application in React-Native Using Offline Database with periodical Online Cloud syncing Experienced and Mastered Nested arrays, Tree structure, Mapping, Promise, Callbacks,Synchronous, Asynchronous, Async Storage, Local Storage, Cookie Handled and solved multiple challenges on Deadlocks and handled Multilevel Exception Handling and Error Handling on both Front-end and Back-End Hands on experience in creating individual Database for particular user-login with a set of Master Data and a Userdefined functions, Indexes, Stored Procedures • Developed C# code for reading the Excel or .xlsxfile and filtering particular columns and push into database ",
+    },
+    {
+      CompanyName: "KDP Tec",
+      specialization: "Software Engineer Intern",
+      YearOfStarting: "Feb2022",
+
+      YearOfLeaving: "May2022",
+      Description:
+        "Working as a Software Engineer Intern on real time project using ReactJs as Front-end MySQL as a Back-end",
+    },
+  ],
+
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "Reactjs and ReactNative",
+    },
+    {
+      skillname: "Asp.Net,C#,MVC",
+    },
+    {
+      skillname: "MySql,Sqlite,Online and Offline Database",
+    },
+  ],
+  // portfolio: [
+  //   {
+  //     name: "project1",
+  //     description: "mobileapp",
+  //     imgurl: "images/portfolio/phone.jpg",
+  //   },
+  //   {
+  //     name: "project2",
+  //     description: "mobileapp",
+  //     imgurl: "images/portfolio/project.jpg",
+  //   },
+  //   {
+  //     name: "project3",
+  //     description: "mobileapp",
+  //     imgurl: "images/portfolio/project2.png",
+  //   },
+  //   {
+  //     name: "project4",
+  //     description: "mobileapp",
+  //     imgurl: "images/portfolio/phone.jpg",
+  //   },
+  // ],
+  testimonials: [
+    {
+      description: "IRIS RECOGNITION USING OR CODE FOR ONLINE ASSESSMENT",
+      name:
+        "This project has been completed in 3 months duration with PHP as front end and My SQL as back end. The application feature of the project is acceptability, speed and reliability constraints of authentication in real application",
+    },
+    {
+      description: "VENTURE PORTAL",
+      name:
+        "This project has been completed in 3 months duration with React Js as front end and My SQL as back end. The web based venture portal provide a description of role based accessibility for control the particular sections of set of venture. Acceptability, speed, and reliability restrictions of authentication in real- world applications are the project's application features",
+    },
+  ],
+};
+  
+  export default resumeData
