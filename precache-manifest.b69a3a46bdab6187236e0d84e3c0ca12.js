9537ebae892ea417c361975189f4d3e770891b00
Shyamprakash01 <pshyam506@gmail.com>
2023-07-12 11:13:13 +0530

    Updates

---
+++ precache-manifest.b69a3a46bdab6187236e0d84e3c0ca12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d65c64b832ffe483c6",
+    "url": "/shyamprakash-portfolio/static/css/main.e72995fa.chunk.css"
+  },
+  {
+    "revision": "98d65c64b832ffe483c6",
+    "url": "/shyamprakash-portfolio/static/js/main.98d65c64.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/shyamprakash-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "05adb83fe0d9d4ca3898",
+    "url": "/shyamprakash-portfolio/static/js/runtime~main.05adb83f.js"
+  },
+  {
+    "revision": "9a1ade647a666bf3f6101aa2085c866e",
+    "url": "/shyamprakash-portfolio/index.html"
+  }
+];
